6c8ee4927e7a335a118eb84f3a11b71f73036539
Derek Sams <dereksams@Dereks-MacBook-Pro.local>
2019-07-14 16:17:31 -0500

    Part 5

---
+++ 5-advanced-JS/starter/script.js
@@ -0,0 +1,17 @@
+// Function constructor
+
+var john = {
+  name: "John",
+  yearOfBirth: 1990,
+  job: teacher
+};
+
+var Person = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+};
+
+var john = new Person("John", 1990, "teacher");
+
+console.log("HEYY");
