ef013abe7ca379a8fcda7ecb47c0bfb7a0012958
Derek Sams <dereksams@Dereks-MacBook-Pro.local>
2019-07-14 20:16:59 -0500

    on video 63

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,17 +1,30 @@
 // Function constructor
 
-var john = {
-  name: "John",
-  yearOfBirth: 1990,
-  job: teacher
-};
+// var john = {
+//   name: "John",
+//   yearOfBirth: 1990,
+//   job: "teacher"
+// };
 
-var Person = function(name, yearOfBirth, job) {
-  this.name = name;
-  this.yearOfBirth = yearOfBirth;
-  this.job = job;
-};
+// var Person = function(name, yearOfBirth, job) {
+//   this.name = name;
+//   this.yearOfBirth = yearOfBirth;
+//   this.job = job;
+// };
 
-var john = new Person("John", 1990, "teacher");
+// Person.prototype.calculateAge = function() {
+//   console.log(2016 - this.yearOfBirth);
+// };
 
-console.log("HEYY");
+// Person.prototype.lastName = "Smith";
+
+// var john = new Person("John", 1990, "teacher");
+// var jane = new Person("Jane", 1969, "designer");
+// var mark = new Person("Mark", 1948, "retired");
+
+// john.calculateAge();
+// jane.calculateAge();
+// mark.calculateAge();
+// console.log(john.lastName);
+
+// Learning Object.create
